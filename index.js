5ba3716f36ee89d7a7409cfda4cd0aee78067e6a
GuillermoCochrane <guilleac81@gmail.com>
2024-07-02 16:42:32 -0300

    Agregadas configuraciones a index

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 //Require Dependecies and modules
 const express = require("express");
+const path = require("path");
+
 //Settings
 const app = express();
 const port = process.env.PORT || 6006;
+app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.urlencoded({ extended: false })); // Required for processing POST method information
+app.use(express.json()); // Required for processing POST method information
+
+//Set Template Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './src/views'));
 
 app.get("/", (req,res)=>{
-    res.send("Home")
+    res.send("Home page")
 });
 
 app.listen(port, ()=>{console.log("\n------------------------------------\nLevantando servidor en puerto " + port +  ": \nhttp://localhost:" + port + "\n------------------------------------\n")
