190b666e0ab6647011fa15e1881dd9d789c8bb9c
GuillermoCochrane <guilleac81@gmail.com>
2024-07-03 20:10:24 -0300

    ruta para el endpoint que devuelve todas las classes de python (/api/python/class)

--- src/Routes/API/pythonApiRoute.js
+++ src/Routes/API/pythonApiRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const mainController = require("../../Controllers/API/pythonApiController")
 
 router.get("/", mainController.index)
+router.get("/class", mainController.classData)
 
 module.exports = router
