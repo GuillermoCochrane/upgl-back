f2fb7b9d07e436786fdc37d85bdd0ab716b347c3
GuillermoCochrane <guilleac81@gmail.com>
2024-07-03 20:05:51 -0300

    implementado endpoint que devuelve el indice del curso de python

--- src/Controllers/API/pythonApiController.js
+++ src/Controllers/API/pythonApiController.js
@@ -1,6 +1,17 @@
+const pythonFunctions = require("../../functions/pythonFunctions")
+
 const pythonApiController = {
     index: function(req,res){
-        return res.render('index',{title:'Python API'})
+        let data = pythonFunctions.fullIndex();
+        let info = {
+            meta: {
+                status : 200,
+                total: data.length,
+                url: 'api/python'
+            },
+            data
+        }
+        return res.json(info)
     },
 }
 module.exports = pythonApiController
