87769586437ddad66d24b79c0ce7bc0a7f506048
GuillermoCochrane <guilleac81@gmail.com>
2024-07-05 01:53:46 -0300

    Implementado endpoint que devuelve el índice del curso de IA

--- src/Controllers/API/iaApiController.js
+++ src/Controllers/API/iaApiController.js
@@ -1,6 +1,17 @@
+const functions = require("../../functions/functions");
+
 const iaApiController = {
     index: function(req,res){
-        return res.render('index',{title:'IA API'})
+        let data = functions.fullIndex("ia");
+        let info = {
+            meta: {
+                status : 200,
+                total_index: data.length,
+                url: 'api/python',
+            },
+            data
+        }
+        return res.json(info);
     },
 }
 module.exports = iaApiController
