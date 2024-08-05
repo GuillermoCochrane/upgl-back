ee4b1ffae3035b57fef11c325e2c71f8f8dcc919
GuillermoCochrane <guilleac81@gmail.com>
2024-08-05 17:10:15 -0300

    Agregagdo endpoint de prueba para todas las clases

--- src/Controllers/API/testApiController.js
+++ src/Controllers/API/testApiController.js
@@ -1,6 +1,7 @@
 const functions = require("../../functions/functions");
 
 const testApiController = {
+
     index: function(req,res){
         let data = functions.fullIndex("test");
         let info = {
@@ -12,6 +13,19 @@ const testApiController = {
             data
         }
         return res.json(info);
+    },
+
+    allClassesData: function(req,res){
+        let data = functions.allEntries("test");
+        let info = {
+            meta: {
+                status : 200,
+                clases: data.length,
+                url: 'api/test/class',
+            },
+            data,
+        }
+        return res.json(info)
     },
 
 }
--- src/Routes/API/testApiRoute.js
+++ src/Routes/API/testApiRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const mainController = require("../../Controllers/API/testApiController");
 
 router.get("/", mainController.index);
+router.get("/class", mainController.allClassesData);
 
 module.exports = router;
