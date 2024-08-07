16b6a95f04fe9b52b9c7db76c63d7f9081b342bc
GuillermoCochrane <guilleac81@gmail.com>
2024-08-07 03:00:50 -0300

    Implementado middleware de ruta para validar datos enviados para la creación de una clase nueva

--- src/Routes/API/testApiRoute.js
+++ src/Routes/API/testApiRoute.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 const mainController = require("../../Controllers/API/testApiController");
 
+//Middlewares
+const classValidations = require("../../middlewares/validations/classesValidationsMDW");
+
 router.get("/", mainController.index);
 router.get("/class", mainController.allClassesData);
 router.get("/class/:classID", mainController.classData);
 router.get("/class/:classID/:topicID", mainController.topicData);
 router.get("/:indexID", mainController.classIndex);
-router.post("/newClass", mainController.newClass);
+router.post("/newClass", classValidations, mainController.newClass);
 router.post("/newTopic/:classID", mainController.newTopic);
 
 module.exports = router;
