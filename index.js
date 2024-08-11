c16ec3bead07978e73059852bfcc00508b06ac8f
GuillermoCochrane <guilleac81@gmail.com>
2024-08-11 00:02:59 -0300

    Eliminadas lineas de código fuera de uso, por eliminar routers en desuso

--- index.js
+++ index.js
@@ -17,15 +17,10 @@ app.set('views', path.join(__dirname, './src/views'));
 //Set Routes
 const mainRouter = require("./src/Routes/indexRoutes");
 const courseApiRouter = require("./src/Routes/API/courseApiRoute");
-// const iaApiRouter = require("./src/Routes/API/iaApiRoute");
-// const pythonApiRouter = require("./src/Routes/API/pythonApiRoute");
-// const testApiRouter = require("./src/Routes/API/testApiRoute");
+
 
 app.get("/", mainRouter);
 app.use("/api/course", courseApiRouter);
-// app.use("/api/ia", iaApiRouter);
-// app.use("/api/python", pythonApiRouter);
-// app.use("/api/test", testApiRouter);
 
 app.listen(port, ()=>{console.log("\n------------------------------------\nLevantando servidor en puerto " + port +  ": \nhttp://localhost:" + port + "\n------------------------------------\n")
 });
