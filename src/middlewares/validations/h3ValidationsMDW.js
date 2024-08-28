4afcb623be0143b706e7384a93b63052c7fc46e3
GuillermoCochrane <guilleac81@gmail.com>
2024-08-28 02:20:50 -0300

    Creada validación de sección H3

---
+++ src/middlewares/validations/h3ValidationsMDW.js
@@ -0,0 +1,13 @@
+const { body } = require('express-validator');
+
+const h3ValidationsMDW = [
+    body('text')
+        .notEmpty().withMessage('Debes completar el título').bail()
+        .isLength({min:2}).withMessage("El titulo del tema debe tener al menos 2 caracteres").bail()
+        .isLength({max:35}).withMessage("El titulo del tema debe tener como máximo 35 caracteres").bail(),
+    body('type')
+        .notEmpty().withMessage('Debes Seleccionar el tipo de contenido').bail(),
+    body('content')
+        .notEmpty().withMessage('Debes Seleccionar el tipo de contenido').bail(),
+]
+module.exports = h3ValidationsMDW
