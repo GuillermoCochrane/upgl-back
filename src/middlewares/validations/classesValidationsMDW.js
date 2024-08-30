0b6bd64bc45cde55165dd348ca99470833c235f0
GuillermoCochrane <guilleac81@gmail.com>
2024-08-30 03:26:23 -0300

    Modiicadas validaciones del middleware

--- src/middlewares/validations/classesValidationsMDW.js
+++ src/middlewares/validations/classesValidationsMDW.js
@@ -3,7 +3,8 @@ const { body } = require('express-validator');
 const classesValidations = [
     body('summary')
         .notEmpty().withMessage('Debes completar el titulo del índice').bail()
-        .isLength({min:2, max:35}).withMessage("El nombre del índice debe tener entre 2 y 35 caracteres"),
+        .isLength({min:2}).withMessage("El nombre del índice debe tener entre al menos 2 caracteres").bail()
+        .isLength({max:35}).withMessage("El nombre del índice debe tener como máximo 35 caracteres"),
     body('title')
         .notEmpty().withMessage('Debes completar el título de la clase').bail()
         .isLength({min:2}).withMessage("El título de la clase debe tener al menos 2 caracteres"),
