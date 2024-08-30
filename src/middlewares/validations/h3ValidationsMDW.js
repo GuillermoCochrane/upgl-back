e99047f1a6fcfb4c1ed4eb7a8400dc527f3304c4
GuillermoCochrane <guilleac81@gmail.com>
2024-08-30 03:29:22 -0300

    Modificadas validaciones del middleware

--- src/middlewares/validations/h3ValidationsMDW.js
+++ src/middlewares/validations/h3ValidationsMDW.js
@@ -3,8 +3,8 @@ const { body } = require('express-validator');
 const h3ValidationsMDW = [
     body('text')
         .notEmpty().withMessage('Debes completar el título').bail()
-        .isLength({min:2}).withMessage("El titulo del tema debe tener al menos 2 caracteres").bail()
-        .isLength({max:35}).withMessage("El titulo del tema debe tener como máximo 35 caracteres").bail(),
+        .isLength({min:2}).withMessage("El título debe tener al menos 2 caracteres").bail()
+        .isLength({max:35}).withMessage("El título debe tener como máximo 35 caracteres"),
     body('type')
         .notEmpty().withMessage('Debes Seleccionar el tipo de sección').bail(),
     body('content')
