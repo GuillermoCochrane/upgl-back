b230b2a0aaec583f1be52d5aa5633ec3f462e199
GuillermoCochrane <guilleac81@gmail.com>
2024-08-31 18:50:30 -0300

    Middleware para validacion de nuevos enlaces

---
+++ src/middlewares/validations/linkValidationsMDW.js
@@ -0,0 +1,16 @@
+const { body } = require('express-validator');
+
+const linkValidationsMDW = [
+    body('text')
+        .notEmpty().withMessage('Debes completar el texto del enlace').bail()
+        .isLength({min:2}).withMessage("El texto del enlace debe tener al menos 2 caracteres").bail(),
+    body('type')
+        .notEmpty().withMessage('Debes Seleccionar el tipo de sección').bail(),
+    body('content')
+        .notEmpty().withMessage('Debes Seleccionar el tipo de contenido').bail(),
+    body('link')
+        .notEmpty().withMessage('Debes completar el enlace').bail()
+        .isLength({min:2}).withMessage("El enlace debe tener al menos 2 caracteres").bail()
+        .isURL().withMessage('Debes ingresar un enlace valido'),
+]
+module.exports = linkValidationsMDW
