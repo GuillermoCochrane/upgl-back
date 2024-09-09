6b8a16c7fdc021053b365b995632327d5f4ca1ac
GuillermoCochrane <guilleac81@gmail.com>
2024-09-09 18:12:15 -0300

    Método para crear el id del li

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -164,6 +164,16 @@ const pythonFunctions = {
         return newSectionID;
     },
 
+    newLiID : function(course, classID, topicID, sectionID){
+        let data = this.allEntries(course);
+        let classTopics = data.filter(lesson => lesson.class == classID)[0].classData;
+        let topicSections = classTopics.filter(topic => topic.topic == topicID)[0].topicData;
+        let section = topicSections.filter(section => section.id == sectionID)[0];
+        let lastLi = section.info.pop();
+        let newLiID = lastLi ? lastLi.liID + 1 : 1;
+        return newLiID;
+    },
+
     newCourse: function(data){
         let allCourses = this.allEntries("courses");
         let id = data.name.split(" ").join("").toLowerCase();
