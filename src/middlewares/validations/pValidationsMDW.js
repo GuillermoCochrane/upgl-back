38816ab37662a0ccdedbdf4124b2a81287c26a6b
GuillermoCochrane <guilleac81@gmail.com>
2024-09-18 18:13:12 -0300

    Modificado middleware de valdación de p

--- src/middlewares/validations/pValidationsMDW.js
+++ src/middlewares/validations/pValidationsMDW.js
@@ -1,12 +1,13 @@
 const { body } = require('express-validator');
 
 const pValidationsMDW = [
-    body('text')
-        .notEmpty().withMessage('Debes completar el título').bail()
-        .isLength({min:2}).withMessage("El título debe tener al menos 2 caracteres").bail(),
     body('type')
-        .notEmpty().withMessage('Debes Seleccionar el tipo de sección').bail(),
-    body('content')
-        .notEmpty().withMessage('Debes Seleccionar el tipo de contenido').bail(),
+        .notEmpty().withMessage('Debes seleccionar un tipo de elemento').bail()
+        .custom((value, {req}) => {
+            if(value != "p"){
+                throw new Error("El tipo de contenido debe ser párrafo (p)");
+            } 
+            return true
+        }) 
 ]
 module.exports = pValidationsMDW
