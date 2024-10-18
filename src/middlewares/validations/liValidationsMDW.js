88731a6cf98850a02d6f71a608332e05cce20235
GuillermoCochrane <guilleac81@gmail.com>
2024-10-18 18:31:26 -0300

    Modificada validacion en el middleware

--- src/middlewares/validations/liValidationsMDW.js
+++ src/middlewares/validations/liValidationsMDW.js
@@ -5,7 +5,7 @@ const liValidationsMDW = [
         .notEmpty().withMessage("Debes seleccionar un tipo de elemento").bail(),
     body("text")
         .notEmpty().withMessage("Debes incluir un texto").bail()
-        .isLength({min:2}).withMessage("El texto debe tener al menos 2 caracteres").bail(),
+        .isLength({min:3}).withMessage("El texto debe tener al menos 3 caracteres").bail(),
     body("content")
         .notEmpty().withMessage("Debes seleccionar un tipo de contenido").bail(),
 ]
