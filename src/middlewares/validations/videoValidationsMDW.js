6148f2d61965a44db66fa5355f147b69c4cc39c6
GuillermoCochrane <guilleac81@gmail.com>
2024-10-19 20:21:01 -0300

    Validaciones de backend para endpoint de creación de videos

---
+++ src/middlewares/validations/videoValidationsMDW.js
@@ -0,0 +1,26 @@
+//Middleware de configuración de express-validator para rutas de usuarios, para imagen de perfil
+const path = require('path');
+const { body } = require('express-validator');
+
+const videoValidationsMDW = [
+    body('title')
+            .notEmpty().withMessage("Debes incluir un título").bail()
+            .isLength({ min: 2 }).withMessage("El título debe tener al menos 2 caracter").bail()
+            .isLength({ max: 30 }).withMessage("El título no puede tener más de 30 caracteres"),
+    body('type')
+        .notEmpty().withMessage('Debes seleccionar el tipo de contenido').bail(),
+    body('video').custom((value, {req}) => {
+        let file = req.file;
+        let acceptedExt = [".avi", ".mp4", ".webm", ".mpg", ".mov"];
+        if(!file){
+            throw new Error("Debes incluir un video");
+        } else {
+            let ext = path.extname(file.originalname)
+            if(!acceptedExt.includes(ext)){
+                throw new Error("El formato del archivo es incompatible");
+            }
+        }
+        return true
+    }) 
+]
+module.exports = videoValidationsMDW
