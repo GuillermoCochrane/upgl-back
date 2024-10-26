037b5d22c8c2b4744f2fec1dc4dcb206c02d29f3
GuillermoCochrane <guilleac81@gmail.com>
2024-10-25 21:22:18 -0300

    Refactorizados métodos addLi y addStub para implentar el método storeSubSection

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -325,88 +325,32 @@ const utilities = {
     },
 
     addLi: function(course, classID, topicID, sectionID, data){
-        let allClasses = this.allEntries(course);
-        let classTopics = allClasses.filter(lesson => lesson.class == classID)[0].classData;
-        let topicSections = classTopics.filter(topic => topic.topic == topicID)[0].topicData;
-        let sectionLis = topicSections.filter(section => section.id == sectionID)[0].info;
-        let newLiID = this.newLiID(course, classID, topicID, sectionID);
+      let newLiID = this.newLiID(course, classID, topicID, sectionID);
 
-        let newLi = {
-            type: data ? data.type : "li",
-            liID: newLiID,
-            order: data ? data.order : newLiID,
-            available: true,
-            stubs: data ? data.info.length  : 0,
-            info:  data ? data.info : [],
-        };
+      let newLi = {
+          type: data ? data.type : "li",
+          liID: newLiID,
+          order: data ? data.order : newLiID,
+          available: true,
+          stubs: data ? data.info.length  : 0,
+          info:  data ? data.info : [],
+      };
 
-        sectionLis.push(newLi);
-
-        for (const section of topicSections){
-            if (section.id == sectionID){
-                section.info = sectionLis;
-                section.stubs = sectionLis.length;
-            }
-        }
-
-        for (const topic of classTopics){
-            if (topic.topic == topicID){
-                topic.topicData = topicSections;
-                topic.sections = topicSections.length;
-            }
-        }
-
-        for (const lesson of allClasses){
-            if (lesson.class == classID){
-                lesson.classData = classTopics;
-                lesson.topics = classTopics.length;
-            }
-        }
-        this.store(allClasses, course);
-
-        return newLi;
+      return this.storeSubSection(course, classID, topicID, sectionID, newLi);
     },
 
     addStub: function(course, classID, topicID, sectionID, data){
-        let allClasses = this.allEntries(course);
-        let classTopics = allClasses.filter(lesson => lesson.class == classID)[0].classData;
-        let topicSections = classTopics.filter(topic => topic.topic == topicID)[0].topicData;
-        let sectionStubs = topicSections.filter(section => section.id == sectionID)[0].info;
-        let newStubID = this.newStubID(course, classID, topicID, sectionID);
+      let newStubID = this.newStubID(course, classID, topicID, sectionID);
 
-        let newStub = {
-            stubID: newStubID,
-            order: data ? data.order : newStubID,
-            available: true,
-            text: data ? data.info[0].text : "",
-            content: data ? data.info[0].content : "plain",
-        };
+      let newStub = {
+        stubID: newStubID,
+        order: data ? data.order : newStubID,
+        available: true,
+        text: data ? data.info[0].text : "",
+        content: data ? data.info[0].content : "plain",
+      };
 
-        sectionStubs.push(newStub);
-
-        for (const section of topicSections){
-            if (section.id == sectionID){
-                section.info = sectionStubs;
-                section.stubs = sectionStubs.length;
-            }
-        }
-
-        for (const topic of classTopics){
-            if (topic.topic == topicID){
-                topic.topicData = topicSections;
-                topic.sections = topicSections.length;
-            }
-        }
-
-        for (const lesson of allClasses){
-            if (lesson.class == classID){
-                lesson.classData = classTopics;
-                lesson.topics = classTopics.length;
-            }
-        }
-        this.store(allClasses, course);
-
-        return newStub;
+      return this.storeSubSection(course, classID, topicID, sectionID, newStub);
     },
 
     newTitle : function(data){
